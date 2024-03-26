9a9b42e7ceb70420c628dca727e9f185e8d1eb60
Shukurillo Baikhanov <71002989+shukurew@users.noreply.github.com>
2024-03-26 15:35:31 +0500

    Update install-gl-v2.sh

--- install-gl-v2.sh
+++ install-gl-v2.sh
@@ -371,7 +371,7 @@ install_greenlight_v3(){
     mkdir -p $GL3_DIR && say "created $GL3_DIR"
   fi
 
-  local GL_IMG_REPO=docker-c2.nls.kz/kenes/fronted-kenes-kz:v1.29
+  local GL_IMG_REPO=docker-c2.nls.kz/kenes/fronted-kenes-kz:v1.30a
 
   say "pulling latest $GL_IMG_REPO image..."
   docker pull $GL_IMG_REPO
